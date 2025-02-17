28f277ad5a841e7f07eb79814f80e242f6d6c2ed
sanaminaaj <86563339+sanaminaaj@users.noreply.github.com>
2025-02-17 21:38:16 +0530

    Create StackMethods.java

---
+++ StackMEthods.java
@@ -0,0 +1,16 @@
+ top = -1;
+    void push(int x) {
+        top++;
+        arr[top] = x;
+    }
+    int pop() {
+        int x = arr[top];
+        top--;
+        return x;
+    }
+    int top() {
+        return arr[top];
+    }
+    int size() {
+        return top + 1;
+    }
